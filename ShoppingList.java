5c80989514cf52ae85393e4ce2005defdb124dba
Kathrine-Voss <s25668@itu.dk>
2026-09-22 15:51:47 +0200

    Creating fields and constructor

--- ShoppingList.java
+++ ShoppingList.java
@@ -1,3 +1,23 @@
-public class ShoppingList {
-    
+import java.util.List;
+
+public class ShoppingList{
+    private int amount;
+    private String unit;
+    private String ingredient;
+    private List available;
+    private List recipe;
+
+
+    public (){
+        this.amount = amount;
+        this.String = unit;
+        this.String = ingredient;
+        this.List =
+
+
+
+
+    }
+
+   
 }
